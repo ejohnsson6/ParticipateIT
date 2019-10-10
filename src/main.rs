bb316e6971ad0c1badaafeda8dcaeede025d4f09
Swexbe <erik@erikjohnsson.se>
2019-10-10 12:24:55 +0200

    Added PartList struct

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,24 @@ use rocket_contrib::json::Json;
 use serde_derive::{Serialize,Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct MyType {
-    test: String,
-    num: i32
+struct PartList {
+    arrangement: String,
+    date: String,
+    part_list: Vec<Participant>
 }
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> Json<MyType> {
-    return Json(MyType{test: "asdas".to_string(), num: 123});
+#[derive(Serialize, Deserialize, Debug)]
+struct Participant {
+    name: String,
+    member: String
 }
 
 #[post("/user", data = "<new_user>")]
-fn new_user(new_user: Json<MyType>) -> &'static str {
+fn new_user(new_user: Json<PartList>) -> &'static str {
     println!("{:?}", new_user);
    return "Test";
 }
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![hello, new_user]).launch();
+    rocket::ignite().mount("/hello", routes![new_user]).launch();
 }
